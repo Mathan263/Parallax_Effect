c679353e23b5be50ef823cb0d87ba72f4ad15741
Mathan263 <65458268+Mathan263@users.noreply.github.com>
2021-10-01 18:40:13 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    
+    // $.ajax({
+    //     url: "snippets/menu.html",
+    //     success: function (response) {
+    //         $('#content').html(response)
+    //     }
+    // });
+
+})
